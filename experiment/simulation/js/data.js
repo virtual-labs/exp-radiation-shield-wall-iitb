d46b1cda9130a557426b7799948621f4e1d1d728
pushpdeep <>
2024-11-26 06:27:58 +0000

    added payload

---
+++ experiment/simulation/js/data.js
@@ -0,0 +1,29 @@
+// common variables
+let sigma = 5.67 * Math.pow(10, -8);
+// activity 1 variables
+let act1_eps1 = 0.4;
+let act1_eps2 = 0.2;
+let act1_eps3 = 0.5;
+let act1_t1_val = Math.floor(Math.random() * (100 + 1)) + 150;
+let act1_t1 = act1_t1_val + 273;
+let act1_t2_val = Math.floor(Math.random() * (10 + 1)) + 25;
+let act1_t2 = act1_t2_val + 273;
+let act1_qaw = 0;
+let act1_qas = 0;
+let act1_redt_per = 0;
+let act1_t3 = 0;
+let act1_ecal = 0;
+let act1_ecal_1 = 0;
+// activity 2 variables
+let act2_epsa = 0.8;
+let act2_epsb = 0.5;
+let act2_epsc = 0.5;
+let act2_epsd = 0.4;
+let act2_ta_val = Math.floor(Math.random() * (100 + 1)) + 500;
+let act2_ta = act2_ta_val + 273;
+let act2_tb_val = Math.floor(Math.random() * (30 + 1)) + 300;
+let act2_tb = act2_tb_val + 273;
+let act2_qa = 0;
+let act2_tc = 0;
+let act2_td = 0;
+//# sourceMappingURL=data.js.map
